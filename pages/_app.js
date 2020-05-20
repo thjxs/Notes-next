632afdc82f0425e8ed74ad908b477feaafa8da6b
thjxs <tanhongjianx@163.com>
2020-05-20 14:14:38 +0800

    Update logo

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import React from "react";
 import App from "next/app";
+import Head from "next/head";
 import "./app.css";
 
 export default class NoteApp extends App {
@@ -7,6 +8,9 @@ export default class NoteApp extends App {
     const { Component, pageProps } = this.props;
     return (
       <div className="h-screen">
+        <Head>
+            <link rel="icon" href="/favicon.ico" />
+        </Head>
         <Component {...pageProps} />
       </div>
     );
