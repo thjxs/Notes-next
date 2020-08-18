cf24230878487f91de01372aec54931dbc1ed72e
thjxs <tanhongjianx@163.com>
2020-08-18 14:31:40 +0800

    upgrade package

--- pages/_app.js
+++ pages/_app.js
@@ -1,30 +1,30 @@
-import React from "react";
-import App from "next/app";
-import Head from "next/head";
-import "./app.css";
-import {DarkModeContext} from '../components/DarkModeContext'
+import React from 'react';
+import App from 'next/app';
+import Head from 'next/head';
+import './app.css';
+import { DarkModeContext } from '@thjxs/gc-markdown';
 
 export default class NoteApp extends App {
   state = {
-    darkMode: false
-  }
+    darkMode: false,
+  };
   componentDidMount() {
-    const mm = window.matchMedia('(prefers-color-scheme: dark)')
+    const mm = window.matchMedia('(prefers-color-scheme: dark)');
     this.setState({
-      darkMode: mm.matches
-    })
+      darkMode: mm.matches,
+    });
 
     mm.addListener((e) => {
-      this.setState({ darkMode: e.matches })
-    })
+      this.setState({ darkMode: e.matches });
+    });
   }
   render() {
-    const {darkMode} = this.state
+    const { darkMode } = this.state;
     const { Component, pageProps } = this.props;
     return (
       <div className="h-screen dark:bg-gray-900 dark:text-gray-100">
         <Head>
-            <link rel="icon" href="/favicon.ico" />
+          <link rel="icon" href="/favicon.ico" />
         </Head>
         <DarkModeContext.Provider value={darkMode}>
           <Component {...pageProps} />
